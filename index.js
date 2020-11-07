86ff4bddd36f699a1418599b3079218966e7fb6e
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 03:00:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(!cardNumber) {
-    return ""
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  let total = total();
+  let message = ``;
+  for(let i = 0; i < cart.length; i++) {
+    
   }
 }
