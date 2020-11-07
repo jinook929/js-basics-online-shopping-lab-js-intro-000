f43bf084a56f9ab826a197b5083a8dcb05a4599b
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 03:01:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   let total = total();
-  let message = ``;
-  for(let i = 0; i < cart.length; i++) {
-    
-  }
+  let message = `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+
+  return message;
 }
