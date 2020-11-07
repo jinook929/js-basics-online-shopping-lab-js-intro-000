dc3d0d0d9863e0192b7feed182d4568ab3e98186
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 02:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,9 +57,12 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  return 
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber) {
+    return ""
+  }
 }
